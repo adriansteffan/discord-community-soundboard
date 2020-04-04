7792790e046a542bcd1985414f3d55fa2aa45f66
adriansteffan <adrian.steffan@hotmail.de>
2020-04-04 16:28:02 +0200

    Add frontend navbar

--- frontend/src/components/bot/BotView.jsx
+++ frontend/src/components/bot/BotView.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './bot.css';
+import Navbar from './navbar/Navbar.jsx'
 
 export default (props) => {
 
 	return (
-		<div>
-			<Toolbox/>
-			<p>This is the main site</p>
-		</div>		
+		<div className = "bot-container">
+			<Navbar/>
+			<p>we need some tabs right here</p>	
+		</div>
 	);
 };
           
--- frontend/src/components/bot/navbar/NavbarView.jsx
+++ frontend/src/components/bot/navbar/NavbarView.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './navbar.css';
 
 export default (props) => {
 
 	return (
 		<div>
-			<Navbar/>
-			<p>This is the main site</p>
+			<nav className="navbar">
+				<ul className="navbar-nav">
+					<li className="nav-item">
+						<FontAwesomeIcon icon={['fas','file-audio']} className="fa-icon"/>
+					</li>
+					<li className="nav-item">
+						<FontAwesomeIcon icon={['fab','youtube']} className="fa-icon"/>
+					</li>
+					<li className="nav-item">
+						<FontAwesomeIcon icon={['fas','upload']} className="fa-icon"/>
+					</li>
+					<li className="nav-item">
+						<FontAwesomeIcon icon={['fas','cogs']} className="fa-icon"/>
+					</li>
+				</ul>
+			</nav>
 		</div>		
 	);
 };

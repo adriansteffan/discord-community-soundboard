4246cbf9598bbc2bb03a3b82fd1790ed7fce2835
Samuel Amar <samuel.amar97@gmail.com>
2020-04-04 23:35:52 +0200

    added part of upload_sound_clip function

--- backend/manage_content/urls.py
+++ backend/manage_content/urls.py
@@ -5,5 +5,6 @@ from manage_content import views
 urlpatterns = [
     path('fetch', views.fetch_data),
     path('create_tag', views.create_tag),
-    path('delete_tag', views.delete_tag)
+    path('delete_tag', views.delete_tag),
+    path('upload_sound_clip', views.upload_sound_clip)
 ]
--- backend/manage_content/views.py
+++ backend/manage_content/views.py
@@ -18,11 +18,12 @@ from manage_content.serializers import CollectionSerializer
 from manage_content.models import Collection
 
 import re
+import inspect, os
+
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def fetch_data(request):
-
     profile = Profile.objects.filter(user=request.user).prefetch_related('guilds')[0]
 
     guilds = GuildSerializer(profile.guilds, many=True).data
@@ -41,12 +42,12 @@ def fetch_data(request):
     }
     return JsonResponse(data, safe=False)
 
+
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 @has_permission('manage_tags')
 @post_fields(['name'])
 def create_tag(request):
-
     name = request.data['name']
 
     if not len(Tag.objects.filter(title=name)) == 0:
@@ -56,17 +57,16 @@ def create_tag(request):
     elif not bool(re.fullmatch(r'\w+', name)):
         return Response('Tag is invalid.')
 
-    tag = Tag(title=name)
-    tag.save()
+    Tag(title=name).save()
 
-    return Response('Tag uploaded successfully.')
+    return Response('Tag created successfully.')
+
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 @has_permission('manage_tags')
 @post_fields(['name'])
 def delete_tag(request):
-
     name = request.data['name']
 
     if len(Tag.objects.filter(title=name)) == 0:
@@ -76,3 +76,19 @@ def delete_tag(request):
 
     return Response('Tag deleted successfully.')
 
+
+# TODO need form-data POST request to upload a file
+@api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+# @has_permission('upload_sound_clip')
+# @post_fields(['name', 'file', 'tags'])
+def upload_sound_clip(request):
+    name = request.data['name']
+    file = request.data['file']
+    tags = request.data['tags']
+
+    #file_path = open(r'C:\Dokumente', 'w')
+    #file_path.write(file)
+    #file_path.close()
+
+    return Response("File '{}' uploaded successfully".format(name))
